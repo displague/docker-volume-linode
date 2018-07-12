4c4a738d0d619343b30f1ddb605250177c744fc1
Ricardo Ramirez <ricardo@rhamerica.com>
2018-07-12 05:56:51 +0000

    testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+set -ev
+
+##############################
 export PLUGIN_NAME_ROOTFS=docker-volume-linode:rootfs-${TRAVIS_BUILD_NUMBER}
 export PLUGIN_NAME=docker-volume-linode:${TRAVIS_BRANCH}.${TRAVIS_BUILD_NUMBER}
 
+##############################
 go get -u github.com/golang/dep/cmd/dep
+##############################
 dep ensure
+##############################
 docker build --no-cache -q -t ${PLUGIN_NAME_ROOTFS} .
+##############################
 mkdir -p ./plugin/rootfs
+##############################
 docker create --name tmp  ${PLUGIN_NAME_ROOTFS}
+##############################
 docker export tmp | tar -x -C ./plugin/rootfs
+##############################
 cp config.json ./plugin/
+##############################
 docker rm -vf tmp
+##############################
 docker plugin rm -f ${PLUGIN_NAME} || true
+##############################
 docker plugin create ${PLUGIN_NAME} ./plugin
+##############################
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+set -ev
+
+##############################
+TRAVIS_BUILD_NUMBER=20
+TRAVIS_BRANCH=testing
+
+##############################
 export PLUGIN_NAME_ROOTFS=docker-volume-linode:rootfs-${TRAVIS_BUILD_NUMBER}
 export PLUGIN_NAME=docker-volume-linode:${TRAVIS_BRANCH}.${TRAVIS_BUILD_NUMBER}
 
+##############################
 echo "$DOCKER_PASSWORD" | docker login -u libgolang --password-stdin
 docker plugin push libgolang/${PLUGIN_NAME}
 
