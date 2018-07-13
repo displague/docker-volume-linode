4def2b50569d265359211e83135facde17a265c2
Ricardo Ramirez <ricardo@rhamerica.com>
2018-07-13 01:06:02 +0000

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,10 +12,12 @@ go get -u github.com/golang/dep/cmd/dep
 ##############################
 dep ensure
 ##############################
+echo "docker build --no-cache -q -t ${PLUGIN_NAME_ROOTFS} ."
 docker build --no-cache -q -t ${PLUGIN_NAME_ROOTFS} .
 ##############################
 mkdir -p ./plugin/rootfs
 ##############################
+echo "docker create --name tmp  ${PLUGIN_NAME_ROOTFS}"
 docker create --name tmp  ${PLUGIN_NAME_ROOTFS}
 ##############################
 docker export tmp | tar -x -C ./plugin/rootfs
@@ -24,9 +26,12 @@ cp config.json ./plugin/
 ##############################
 docker rm -vf tmp
 ##############################
+echo "docker plugin rm -f ${PLUGIN_NAME} || true"
 docker plugin rm -f ${PLUGIN_NAME} || true
 ##############################
+echo "docker plugin create ${PLUGIN_NAME} ./plugin"
 docker plugin create ${PLUGIN_NAME} ./plugin
 ##############################
+echo "docker save -o ${PLUGIN_NAME_TAR} ${PLUGIN_NAME}"
 docker save -o ${PLUGIN_NAME_TAR} ${PLUGIN_NAME}
 ##############################
